6f9c99156ef3c662b168fa5019ca6ebea34113a0
WpythoN <ustinipad@gmail.com>
2021-10-01 21:05:41 +0300

    Начальный возврат модуля lab_1.4

---
+++ lab_1.4/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+namespace lab_1._4
+{ 
+    class A
+    {
+        private B b = null;
+        private C c = null;
+        private J j = null;
+
+        public A(B b, C c, J j)
+        {
+            this.b = b;
+            this.c = c;
+            this.j = j;
+        }
+
+        public void mA()
+        {
+            Console.WriteLine("method of A");
+        }
+
+        public B bA
+        {
+            set { Console.WriteLine("set b"); b = value; }
+            get { Console.Write("get b -> "); return b; }
+        }
+        public C cA //атрибут доступа к атрибутам
+        {
+            set { Console.WriteLine("set c"); c = value; }
+            get { Console.Write("get c -> "); return c; }
+        }
+        public J jA
+        {
+            set { Console.WriteLine("set j"); j = value; }
+            get { Console.Write("get j -> "); return j; }
+        }
+
+
+    }
+
+    class B
+    {
+        private K k = null;
+        private D d = null;
+
+        public B(K k, D d)
+        {
+            this.k = k;
+            this.d = d;
+        }
+
+        public void mB()
+        {
+            Console.WriteLine("method of B");
+        }
+
+        public K kA
+        {
+            set { Console.WriteLine("set k"); k = value; }
+            get { Console.Write("get k -> "); return k; }
+        }
+
+        public D dA
+        {
+            set { Console.WriteLine("set d"); d = value; }
+            get { Console.Write("get d -> "); return d; }
+        }
+
+    }
+
+    class C
+    {
+        private E e = null;
+
+        public C(E e)
+        {
+            this.e = e;
+        }
+
+        public void mC()
+        {
+            Console.WriteLine("method of C");
+        }
+
+        public E eA
+        {
+            set { Console.WriteLine("set e"); e = value; }
+            get { Console.Write("get e -> "); return e; }
+        }
+
+    }
+
+    class J
+    {
+        private F f = null;
+
+        public J(F f)
+        {
+            this.f = f;
+        }
+
+        public void mJ()
+        {
+            Console.WriteLine("Method of J");
+        }
+
+        public F fA
+        {
+            set { Console.WriteLine("set e"); f = value; }
+            get { Console.Write("get e -> "); return f; }
+        }
+
+
+    }
+
+    class K
+    {
+        public K() { }
+
+        public void mK()
+        {
+            Console.WriteLine("Method of K");
+        }
+
+    }
+
+    class D
+    {
+        public D() { }
+
+        public void mD()
+        {
+            Console.WriteLine("Method of D");
+        }
+    }
+
+    class E
+    {
+        public E() { }
+
+        public void mE()
+        {
+            Console.WriteLine("Method of E");
+        }
+    }
+
+    class F
+    {
+        public F() { }
+
+        public void mF()
+        {
+            Console.WriteLine("Method of F");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task: ");
+            D d = new D();
+            E e = new E();
+            F f = new F();
+            J j = new J(f);
+            K k = new K();
+            B b = new B(k, d);
+            C c = new C(e);
+            A a = new A(b, c, j);
+            Console.WriteLine("Передача по ссылке: ");
+            a.mA();
+            a.jA.mJ();
+            a.cA.mC();
+            a.bA.mB();
+            a.bA.kA.mK();
+            a.bA.dA.mD();
+            a.cA.eA.mE();
+            a.jA.fA.mF();   //в объекте класса а обращаемся к атрибуту доступа, который возвращает значение. Это значение - ссылка на объект другого класса, в котором мы обращаемся к методу
+            Console.ReadKey();
+
+        }
+    }
+
+}
